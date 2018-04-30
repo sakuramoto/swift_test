f1036ac4dc0abce652cbceee62d3e64ef049d7fc
sakuramoto <sakura@151a.jp>
2018-04-30 10:08:17 +0900

    プロトタイプ完成

---
+++ NewsReader/NewsReader/Item.swift
@@ -0,0 +1,12 @@
+//
+//  Item.swift
+//  NewsReader
+//
+//  Created by Takashi Sakuramoto on 2018/04/27.
+//  Copyright © 2018年 151a.jp. All rights reserved.
+//
+
+class Item {
+    var title = ""
+    var link = ""
+}
--- NewsReader/NewsReader/ListViewController.swift
+++ NewsReader/NewsReader/ListViewController.swift
@@ -8,14 +8,85 @@
 
 import UIKit
 
-class ListViewController: UITableViewController {
+//class ListViewController: UITableViewController {
+//
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 3
+//    }
+//
+//    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//        return cell
+//    }
+//}
+
+class ListViewController : UITableViewController, XMLParserDelegate {
+    
+    var parser:XMLParser!
+    var items = [Item]()
+    var item:Item?
+    var currentString = ""
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        
+        return items.count
+    
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = items[indexPath.row].title
+
         return cell
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        startDownload()
+    }
+    
+    func startDownload() {
+        
+        self.items = []
+        print("start download")
+        if let url = URL (string: "https://www.apple.com/jp/newsroom/rss-feed.rss"){
+            print("start parse")
+            if let parser = XMLParser(contentsOf: url) {
+                print("start parsing")
+                self.parser = parser
+                self.parser.delegate = self
+                self.parser.parse()
+            }
+        }
+    }
+    
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+        
+        self.currentString = ""
+        if elementName == "item" {
+
+            self.item = Item()
+
+        }
+    }
+    
+    func parser(_ parser: XMLParser, foundCharacters string: String) {
+        self.currentString += string
+    }
+    
+    func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        
+        switch elementName {
+        case "title": self.item?.title = currentString
+        case "link": self.item?.link = currentString
+        case "item": self.items.append(self.item!)
+        default: break
+        }
+        
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        self.tableView.reloadData()
     }
 }
