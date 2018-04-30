3978ef5050d8a9d2dbaecb050af18b6dde1e132b
sakuramoto <sakura@151a.jp>
2018-04-30 10:22:51 +0900

    add navigation controller

---
+++ NewsReader/NewsReader/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  NewsReader
+//
+//  Created by Takashi Sakuramoto on 2018/04/30.
+//  Copyright © 2018年 151a.jp. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController : UIViewController {
+    
+}
