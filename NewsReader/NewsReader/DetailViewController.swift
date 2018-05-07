73fb34adcad288dd2963079d8d1df8c05ad4c376
sakuramoto <sakura@151a.jp>
2018-05-07 13:38:08 +0900

    add: webview

--- NewsReader/NewsReader/DetailViewController.swift
+++ NewsReader/NewsReader/DetailViewController.swift
@@ -10,4 +10,15 @@ import UIKit
 
 class DetailViewController : UIViewController {
     
+    @IBOutlet weak var webView: UIWebView!
+    
+    var link:String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = URL(string: self.link) {
+            let request = URLRequest(url: url)
+            self.webView.loadRequest(request)
+        }
+    }
 }
